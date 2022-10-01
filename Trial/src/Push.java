7f4187a9c6556144bb3fe7507fc816ec9c8f9b3f
Rachit-Chandra <rachitchandra065@gmail.com>
2022-10-01 16:56:56 +0530

    Commit-1

---
+++ Trial/src/Push.java
@@ -0,0 +1,12 @@
+
+public class Push {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i =0;i<10000000;i++) {
+		System.out.println("sorry Baby");
+		}
+	}
+
+}
